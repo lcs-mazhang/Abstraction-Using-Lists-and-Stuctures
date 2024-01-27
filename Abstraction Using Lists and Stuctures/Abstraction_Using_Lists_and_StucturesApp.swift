52b3bab602bbee6cf8329711294f6095d31d38ea
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-27 09:58:32 -0500

    Initial Commit

---
+++ Abstraction Using Lists and Stuctures/Abstraction_Using_Lists_and_StucturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Abstraction_Using_Lists_and_StucturesApp.swift
+//  Abstraction Using Lists and Stuctures
+//
+//  Created by GengYu Zhang on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct Abstraction_Using_Lists_and_StucturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
